9d11c18345d6702b9d853599d682d80f3f2033d4
Romanojp <jp_romana@outlook.com>
2023-10-05 18:27:19 -0300

    first commit

---
+++ minhas-tarefas/src/main/java/br/com/uniciv/tarefas/controller/TarefaController.java
@@ -0,0 +1,9 @@
+package br.com.uniciv.tarefas.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TarefaController {
+	
+
+}
---
+++ minhas-tarefas/src/main/java/br/com/uniciv/tarefas/model/TarefaStatus.java
@@ -0,0 +1,6 @@
+package br.com.uniciv.tarefas.model;
+
+public enum TarefaStatus {
+	
+	ABERTO, EM_ANDAMENTO, CONCLUIDA, CANCELADA
+}
